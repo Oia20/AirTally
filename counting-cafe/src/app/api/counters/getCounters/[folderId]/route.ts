8973763eadef8869b29471fbd2c238ed6af60212
Jacob <92279567+Oia20@users.noreply.github.com>
2024-11-25 20:09:46 -0600

    Update route.ts

--- counting-cafe/src/app/api/counters/getCounters/[folderId]/route.ts
+++ counting-cafe/src/app/api/counters/getCounters/[folderId]/route.ts
@@ -2,11 +2,11 @@ import { prisma } from "../../../../lib/prisma/prisma";
 
 export async function GET(
   req: Request,
-  context : { params: { folderId: string } }
+  context : { params: Promise<{ folderId: string }> }
 ) {
   const params = await context.params;
   const folderId = params.folderId;
-  
+
   if (!folderId) return new Response('Folder ID is required', { status: 400 });
 
   const counters = await prisma.counters.findMany({
