12ac626445cda9ad0e5ee12332bda5799d30d9d2
Jacob <92279567+Oia20@users.noreply.github.com>
2024-11-25 20:11:32 -0600

    Build errors now resolved. params needed to be wrapped in the Promise type.

--- counting-cafe/src/app/api/folders/getFolders/[userId]/route.ts
+++ counting-cafe/src/app/api/folders/getFolders/[userId]/route.ts
@@ -2,7 +2,7 @@ import { prisma } from "../../../../lib/prisma/prisma";
 
 export async function GET(
   req: Request,
-  context: { params: { userId: string } }
+  context: { params: Promise<{ userId: string }> }
 ) {
   const params = await context.params;
   const userId = params.userId;
